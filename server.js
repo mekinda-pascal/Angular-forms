682ef720fffd3801ef535bf2782d820509adefd3
mekinda-pascal <77722939+mekinda-pascal@users.noreply.github.com>
2022-03-08 18:43:40 +0100

    parametrage pour deployment de l'appli sur heroku

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+const express = require("express");
 const path = require("path");
-const { dirname } = require("path/posix");
+//const { dirname } = require("path/posix");
+
 const app = express();
 
 app.use(express.static(__dirname + "/dist/angular-forms"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(pass.join(dirname + "/dist/angular-forms/login"));
+  res.sendFile(path.join(dirname + "/dist/angular-forms/login"));
 });
 
 app.listen(process.env.PORT || 3000);
